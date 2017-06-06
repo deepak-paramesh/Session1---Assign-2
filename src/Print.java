3108091f987b76528a3877016aa676fe4ed9521f
pade6001 <pade6001@01hw332474.mshome.net>
2017-06-06 19:44:14 +0530

    Session1 Assign 2

---
+++ src/Print.java
@@ -0,0 +1,20 @@
+
+public class Print {
+
+	public static void main(String[] args) {
+		int a  = 20;
+		int b = 10;
+		
+		 b= a-- - --a;
+		 System.out.println("b :" +b);
+		int c=a--;
+		System.out.println("c : " +c);
+		int d=a>>2;
+		System.out.println("d : " +d);
+		int e=a&b;
+		System.out.println("e : " +e);
+		System.out.println();
+
+	}
+
+}
